6c5f0cdd5552a0a099c42eecde0cdeaea98f8c49
FUT4KUCHi <edddy.management@protonmail.com>
2025-02-10 04:30:45 +0630

    Removed unnecessary files.

--- scripts/player.tres.gd
+++ scripts/player.tres.gd
@@ -1,7 +1,4 @@
-[gd_scene load_steps=4 format=3 uid="uid://dvyodnoqs625h"]
-
-[sub_resource type="GDScript" id="GDScript_yjyad"]
-script/source = "extends CharacterBody3D
+extends CharacterBody3D
 
 const mouse_sens = 0.2
 const SPEED = 5.0
@@ -30,28 +27,28 @@ func _input(event):
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed(\"escape-debug\"):
+	if Input.is_action_just_pressed("escape-debug"):
 		get_tree().quit()
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	if toggle_flashlight: 
-		if Input.is_action_just_pressed(\"ui_accept\"):
+		if Input.is_action_just_pressed("ui_accept"):
 			flashlight.visible = false
 			toggle_flashlight = false
 	elif !toggle_flashlight:
-		if Input.is_action_just_pressed(\"ui_accept\"):
+		if Input.is_action_just_pressed("ui_accept"):
 			flashlight.visible = true
 			toggle_flashlight = true
 
 	# Handle jump. (INPUT NOT ASSIGNED)
-	if Input.is_action_just_pressed(\"space_bar\") and is_on_floor():
+	if Input.is_action_just_pressed("space_bar") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector(\"ui_left\", \"ui_right\", \"ui_up\", \"ui_down\")
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -70,20 +67,3 @@ func _headbob(time) -> Vector3:
 	var pos = Vector3.ZERO
 	pos.y = sin(time * BOB_FREQ) * BOB_AMP
 	return pos
-"
-
-[sub_resource type="CapsuleMesh" id="CapsuleMesh_8opcf"]
-
-[sub_resource type="CapsuleShape3D" id="CapsuleShape3D_pqise"]
-
-[node name="player" type="CharacterBody3D"]
-transform = Transform3D(1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0.3, 0)
-script = SubResource("GDScript_yjyad")
-
-[node name="MeshInstance3D" type="MeshInstance3D" parent="."]
-transform = Transform3D(1.3, 0, 0, 0, 1.3, 0, 0, 0, 1.3, 0, 0.996699, 0)
-mesh = SubResource("CapsuleMesh_8opcf")
-
-[node name="CollisionShape3D" type="CollisionShape3D" parent="."]
-transform = Transform3D(1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0)
-shape = SubResource("CapsuleShape3D_pqise")
